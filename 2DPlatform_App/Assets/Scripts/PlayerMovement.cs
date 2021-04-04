c1454d64f54f91324bd6f78c5b57b5cdf68eb2a4
Tarek <elhaj102@mail.chapman.edu>
2021-04-04 14:37:24 -0700

    have a player that can move via horizontal key inputs

---
+++ 2DPlatform_App/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 25f;
+
+    private float horizontalMove = 0f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
